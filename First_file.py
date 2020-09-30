3d90427c4c07399830aa583bb100edb5785f0537
Rinat <plavnik.ra@phystech.edu>
2020-09-30 17:04:19 +0300

    Сделал домашнее за первую неделю. См. файл week01_homework_RinatPlavnik.ipynb

--- First_file.py
+++
@@ -1,4 +0,0 @@
-print("First line in First file in branch Master")
-print("Second line in First file in branch Master")
-print("Third line in First file in branch Master")
-print("This is new line in New_branch")
