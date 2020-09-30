bde12812579f7d807d8ecf8eeb4e54bf890800fb
Rinat <plavnik.ra@phystech.edu>
2020-09-30 12:31:38 +0300

    changed First_file.py

--- First_file.py
+++ First_file.py
@@ -1,4 +1,3 @@
 print("First line in First file in branch Master")
 print("Second line in First file in branch Master")
 print("Third line in First file in branch Master")
-print("Second line in First file in branch Third_branch")
