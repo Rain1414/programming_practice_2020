2ed7442308792f2b3b92c621f54e0cf6a52d2f24
Rinat <plavnik.ra@phystech.edu>
2020-09-30 12:33:57 +0300

    Added new line in First_file.py in branch - Master

--- First_file.py
+++ First_file.py
@@ -1,3 +1,4 @@
 print("First line in First file in branch Master")
 print("Second line in First file in branch Master")
 print("Third line in First file in branch Master")
+print("this is new line in branch - Master")
